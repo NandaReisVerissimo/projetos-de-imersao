b49321eb7e0757848583fc9dc7ab31267288d210
NandaReisVerissimo <nandareis83@gmail.com>
2022-09-07 08:58:00 -0300

    funcionamento do conversor

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-function converter() {
+function converter () {
     let grausCelsius = document.getElementById ("valor")
-    let temperatura = grausCelsius.value
-    let temperaturaDecimal = parseFloat (temperatura)
-    let grausFahrenheit = temperaturaDecimal * 1.8 + 32
+    let grausCelsiusValor = grausCelsius.value
+    let grausCelsiusDecimal = parseFloat (grausCelsiusValor)
+    let grausFahrenheit = grausCelsiusDecimal * 1.8 + 32
 
     console.log (grausFahrenheit)
+
+    let grauConvertido = document.getElementById ("tempConvertida")
+    let valorConvertido = "A Temperatura é " + grausFahrenheit + " °F"
+    grauConvertido.innerHTML = valorConvertido
 }
+
+//pegar o valor do input
+//transformar em valor
+//definir como decimal
+//fazer a conta
+//gerar o resultado
+//mostrar o resultado na tela
