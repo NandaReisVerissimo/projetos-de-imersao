4eee0b45d466597425ca2f73b6279a25dd7b5cdd
NandaReisVerissimo <nandareis83@gmail.com>
2022-09-12 15:41:36 -0300

    alura

---
+++ Alura-Flix/script.js
@@ -0,0 +1,22 @@
+//aparecer a imagem dos filmes favoritos na nossa tela
+let listaFilmes = []
+
+function compartilhar() {
+    let filmeCompartilhado = document.getElementById ("url").value;
+    if (filmeCompartilhado.endsWith(".jpg")) { listarFilmesNaTela(filmeCompartilhado)
+    } else {
+      alert("Endereço de Filme Inválido")
+    }
+    document.getElementById ("url").value = ""
+  }
+  
+  function listarFilmesNaTela(filme) {
+    let elementoFilmeFavorito = "<img src=" + filme + ">"
+    let elementoListaFilmes = document.getElementById("compartilhados")
+    elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoFilmeFavorito
+  }
+
+  function adicionarNome() {
+    let filmeAdicionado = document.getElementById ("nomeFilme").value;
+    document.getElementById ("nomeFilme").value = ""
+  }
